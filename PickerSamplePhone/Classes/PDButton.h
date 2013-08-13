abdb362c0880b8c6dff47363ea92bb5a36c5098a
Giacomo <gsaccardo@gmail.com>
2013-08-13 11:14:14 +0100

    Added PDButtons instead of standard UIBarButtonItem

---
+++ PickerSamplePhone/Classes/PDButton.h
@@ -0,0 +1,13 @@
+//
+//  PDButton.h
+//  Pin Drop
+//
+//  Created by Giacomo Saccardo on 12/08/2013.
+//  Copyright (c) 2013 Caffeinehit Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDButton : UIButton
+
+@end
---
+++ PickerSamplePhone/Classes/PDButton.m
@@ -0,0 +1,51 @@
+//
+//  PDButton.m
+//  Pin Drop
+//
+//  Created by Giacomo Saccardo on 12/08/2013.
+//  Copyright (c) 2013 Caffeinehit Ltd. All rights reserved.
+//
+
+#import "PDButton.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation PDButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.layer.borderWidth = 1;
+        self.layer.borderColor = [UIColor colorWithWhite:1.0 alpha:0.2].CGColor;
+        self.layer.cornerRadius = 3;
+        
+        [self addTarget:self action:@selector(highlightBorder) forControlEvents:UIControlEventTouchDown];
+        [self addTarget:self action:@selector(unhighlightBorder) forControlEvents:UIControlEventTouchUpInside];
+        [self addTarget:self action:@selector(unhighlightBorder) forControlEvents:UIControlEventTouchDragOutside];
+    }
+    return self;
+}
+
+- (void)highlightBorder
+{
+    self.layer.borderColor = [UIColor colorWithWhite:1.0 alpha:0.5].CGColor;
+}
+
+- (void)unhighlightBorder
+{
+    self.layer.borderColor = [UIColor colorWithWhite:1.0 alpha:0.2].CGColor;
+}
+
+- (void)setEnabled:(BOOL)enabled {
+    
+    [super setEnabled:enabled];
+    
+    if (enabled) {
+        self.titleLabel.alpha = 1.0;
+    }
+    else {
+        self.titleLabel.alpha = 0.5;
+    }
+}
+
+@end
